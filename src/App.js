194d1672395a84f913c1ac186e1ea776304fb919
devilyn <devilyn0216@gmail.com>
2023-05-19 00:30:19 +0900

    ~19강 (https://www.youtube.com/watch?v=m8A02RtmWmA&list=PLB7CpjPWqHOs7ZCQfvA63dkV2A5MRyTRk&index=20)

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import BlogForm from "./components/BlogForm";
+import NavBar from "./components/NavBar";
+
+function App() {
+    return (
+        <Router>
+            <NavBar />
+            <div className="container">
+                <Switch>
+                    <Route path="/" exact>
+                        Home Page
+                    </Route>
+                    <Route path="/blogs">
+                        <BlogForm />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+
+    )
+}
+
+export default App;
