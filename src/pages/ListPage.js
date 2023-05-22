d850b73cbfc8426189a7aabfbdaca3e80b5cbd8e
devilyn <devilyn0216@gmail.com>
2023-05-23 00:58:54 +0900

    ~30강 (https://www.youtube.com/watch?v=u5RTBG1Ians&list=PLB7CpjPWqHOs7ZCQfvA63dkV2A5MRyTRk&index=33)

---
+++ src/pages/ListPage.js
@@ -0,0 +1,55 @@
+import axios from "axios";
+import {useEffect, useState} from "react";
+import Card from "./Card";
+import {Link, useHistory} from "react-router-dom";
+
+const ListPage = () => {
+    const history = useHistory();
+    const [posts, setPosts] = useState([]);
+
+    const getPosts = () => {
+        axios.get('http://localhost:3001/posts').then((res) => {
+            setPosts(res.data);
+        });
+    };
+
+    useEffect(() => {
+        getPosts();
+    }, []);
+
+    return (
+        <div>
+            <div className="d-flex justify-content-between">
+                <h1>Blogs</h1>
+                <div>
+                    <Link to="/blogs/create" className="btn btn-success">
+                        Create New
+                    </Link>
+                </div>
+            </div>
+            {posts.map(post => {
+                return (
+                    <Card
+                        key={post.id}
+                        title={post.title}
+                        onClick={() => history.push('/blogs/edit')}
+                    >
+                        <div>
+                            <button
+                                className="btn btn-danger btn-sm"
+                                onClick={(e) => {
+                                    e.stopPropagation();
+                                    console.log('delete blogs')
+                                }}
+                            >
+                                Delete
+                            </button>
+                        </div>
+                    </Card>
+                );
+            })}
+        </div>
+    );
+};
+
+export default ListPage;
