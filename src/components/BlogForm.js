2fdd9a1f95e95e7f5e1f1bbcb10bcc68c4b0a2c3
devilyn <devilyn0216@gmail.com>
2023-06-08 01:01:53 +0900

    69 Toast Notification 6 (https://www.youtube.com/watch?v=B5W9RNvhHtw&list=PLB7CpjPWqHOs7ZCQfvA63dkV2A5MRyTRk&index=74)

--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -2,11 +2,8 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 import {useHistory, useParams} from "react-router-dom";
 import propTypes from "prop-types";
-import Toast from "./Toast";
-import useToast from "../hooks/toast";
 
-const BlogForm = ({editing}) => {
-    const [toasts, addToast, deleteToast] = useToast();
+const BlogForm = ({editing, addToast}) => {
     const history = useHistory();
     const { id } = useParams();
 
@@ -87,7 +84,7 @@ const BlogForm = ({editing}) => {
                         type: 'success',
                         text: 'Successfully created!'
                     });
-                    // history.push('/admin');
+                    history.push('/admin');
                 });
             }
         }
@@ -100,10 +97,6 @@ const BlogForm = ({editing}) => {
 
     return(
         <div>
-            <Toast
-                toasts={toasts}
-                deleteToast={deleteToast}
-            />
             <h1>{editing ? 'Edit' : 'Create'} a blog post</h1>
             <div className="mb-3">
                 <label className="form-label">Title</label>
